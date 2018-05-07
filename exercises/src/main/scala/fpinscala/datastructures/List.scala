384bdeb5d25cfb8f68155272e75c3ee9f10c41d6
Daniel Kimbel <daniel.kimbel@lendup.com>
2018-05-06 22:32:06 -0700

    Ch3 exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,19 +50,167 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(_, t) => t
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(_, t) => Cons(h, t)
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n == 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(h, t) =>
+        if (f(h)) dropWhile(t, f)
+        else l
+    }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = {
+    foldRight(l, 0)((_, z) => z + 1)
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B =
+    l match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def sumLeft(l: List[Int]): Int =
+    foldLeft(l, 0)(_ + _)
+
+  def productLeft(l: List[Double]): Double =
+    foldLeft(l, 1.0)(_ * _)
+
+  def lengthLeft[A](l: List[A]): Int =
+    foldLeft(l, 0)((z, _) => z + 1)
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil: List[A])((as, a) => Cons(a, as))
+
+  def appendItem[A](l: List[A], a: A): List[A] =
+    foldRight(l, Cons(a, Nil))(Cons(_, _))
+
+  def appendList[A](l1: List[A], l2: List[A]): List[A] =
+    foldRight(l1, l2)(Cons(_, _))
+
+  def concat[A](ls: List[List[A]]): List[A] =
+    foldRight(ls, Nil: List[A])(appendList)
+
+  // Note: having looked at the answer key, my implementations are not quite correct.
+  // They do work for division, but I suppose that they might break if I tried to do
+  // any kind of short-circuiting -- they would start from the wrong end of the list,
+  // and thus short-circuit earlier or later than a TRUE fold left or right would have.
+  def foldLeft2[A, B](l: List[A], z: B)(f: (B, A) => B) =
+    foldRight(l, z)((a, b) => f(b, a))
+
+  def foldRight2[A, B](l: List[A], z: B)(f: (A, B) => B) =
+    foldLeft(l, z)((a, b) => f(b, a))
+
+  def addOne(l: List[Int]): List[Int] =
+    foldRight(l, Nil: List[Int])((h, t) => Cons(h + 1, t))
+
+  def stringify(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((h, t) => Cons(h.toString, t))
+
+  // first I wrote it in terms of foldRight, then without using a fold
+  def map[A,B](l: List[A])(f: A => B): List[B] =
+    foldRight(l, Nil: List[B])((h, t) => Cons(f(h), t))
+
+  def myOtherMap[A,B](l: List[A])(f: A => B): List[B] =
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(f(h), map(t)(f))
+    }
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] =
+    l match {
+      case Nil => Nil
+      case Cons(h, t) =>
+        if (f(h)) Cons(h, filter(t)(f))
+        else filter(t)(f)
+    }
+
+  def myOtherFilter[A](l: List[A])(f: A => Boolean): List[A] =
+    foldRight(l, Nil: List[A])((h, z) =>
+      if (f(h)) Cons(h, z)
+      else z
+    )
+
+  def onlyEven(l: List[Int]): List[Int] =
+    filter(l)(n => n % 2 == 0)
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+    }
+  }
+
+  def myOtherFlatMap[A, B](l: List[A])(f: A => List[B]): List[B] = {
+    foldRight(l, Nil: List[B])((a, bs) =>
+      append(f(a), bs)
+    )
+  }
+
+  // Am I missing something, or am I right that using flatMap to implement filter is a
+  // really weird idea? I'm interpreting this as an intellectual exercise, rather than
+  // an actual deep connection between filter and flatMap.
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] =
+    flatMap(l)(a =>
+      if (f(a)) Cons(a, Nil)
+      else Nil
+    )
+
+  def addListContents(xs: List[Int], ys: List[Int]): List[Int] =
+    (xs, ys) match {
+      case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, addListContents(xs, ys))
+      case (_, _) => Nil
+    }
+
+  def zipWith[X, Y, Z](xs: List[X], ys: List[Y])(f: (X, Y) => Z): List[Z] =
+    (xs, ys) match {
+      case (Cons(x, xt), Cons(y, yt)) => Cons(f(x, y), zipWith(xt, yt)(f))
+      case (_, _) => Nil
+    }
+
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+    // iterate through sup; at each step, take sub.length from sup
+    // and compare it to sub for equality
+    val subLen = length(sub)
+    def go[B](as: List[B]): Boolean =
+      as match {
+        case Nil => false
+        case Cons(_, t) =>
+          if (take(as, subLen) == sub) true
+          else go(t)
+      }
+
+    go(sup)
+  }
+
+  def take[A](l: List[A], n: Int): List[A] =
+    l match {
+      case Cons(h, t) if n > 0 => Cons(h, take(t, n - 1))
+      case _ => Nil
+    }
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,69 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  def size[A](t: Tree[A]): Int =
+    t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + size(l) + size(r)
+    }
 
+  def maximum(t: Tree[Int]): Int =
+    t match {
+      case Leaf(n) => n
+      case Branch(l, r) => maximum(l).max(maximum(r))
+    }
 
+  def depth[A](t: Tree[A]): Int = {
+    // I need two things to be happening: I need to add one to a depth
+    // that I'm actively keeping track of with each branching level, AND
+    // I need to select `max` values between different branches. I can do
+    // this either in two passes (map depth onto a tree, then find its max
+    // value) or in one pass. I'm going to attempt one pass here.
+    def go[B](t: Tree[B], d: Int): Int =
+      t match {
+        case Leaf(_) => d
+        case Branch(l, r) => go(l, d + 1).max(go(r, d + 1))
+      }
 
+    go(t, 0)
+  }
 
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    t match {
+      case Leaf(a) => Leaf(f(a))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+
+  // Note: my folds are like a list's foldRight, in that they pass control back and forth
+  // between fold and a function argument (in this case, g). This allows for short-circuiting,
+  // which is a strength, but it means that this function is not tail-recursive and therefore
+  // is not stack-safe. However, while it's a little hard for me to tell without digging deeper,
+  // I think that my implementation might actually be left associative.
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B =
+    t match {
+      case Leaf(a) => f(a)
+      case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    }
+
+  // The great news is that my fold is just like the book's solution. The bad news is that
+  // my foldZ was actually unnecessary for the implementation of depthViaFold; instead of
+  // using (_ max _) as g, you can use ((b1, b2) => 1 + (b1 max b2)).
+  def foldZ[A, B](t: Tree[A], z: B)(f: (A, B) => B)(g: (B, B) => B)(h: B => B): B =
+    (t, z) match {
+      case (Leaf(a), acc) => f(a, acc)
+      case (Branch(l, r), acc) => g(foldZ(l, h(acc))(f)(g)(h), foldZ(r, h(acc))(f)(g)(h))
+    }
+
+  def sizeViaFold[A](t: Tree[A]): Int =
+    fold(t)((_) => 1)(_ + _ + 1)
+
+  def maximumViaFold(t: Tree[Int]): Int =
+    fold(t)(identity)(_ max _)
+
+  def depthViaFold[A](t: Tree[A]): Int =
+    foldZ(t, 0)((_, b) => b)(_ max _)(_ + 1)
+  //    fold(t)(_ => 0)((b1, b2) => 1 + (b1 max b2)) // the book's superior solution
+
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])((b1, b2) => Branch(b1, b2))
 }
