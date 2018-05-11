de8273ed5e8c59c8a0fe1b1913c90bc63d98bdf5
Daniel Kimbel <daniel.kimbel@lendup.com>
2018-05-11 09:06:23 -0700

    Ch4 exercises

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,24 +1,52 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
+import scala.{Either => _, Left => _, Option => _, Right => _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  // I don't see any simple way to implement this without using pattern matching,
+  // because we haven't defined any catamorphism to break down the extra either
+  // structure introduced by the map call.
+  // (Indeed, the book's solution also resorted to pattern matching.)
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(a) => f(a)
+      case Left(e) => Left(e)
+    }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Right(_) => this
+      case Left(_) => b
+    }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  // I'm basically copying the book's Option solution, but from my own memory --
+  // I remember that it chains flatMap and map, with map as the more deeply nested call
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    this flatMap (aa => b map (bb => f(aa, bb)))
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  // I'm doing this from memory of my folding solution for Option.traverse
+  def traverse[E,A,B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as.foldRight(Right(Nil): Either[E, List[B]])((ha, z) => f(ha).map2(z)(_ :: _))
+    // an alternative using a for comprehension:
+//    as.foldRight(Right(Nil): Either[E, List[B]])((ha, z) =>
+//      for {
+//        bb <- f(ha)
+//        zz <- z
+//      } yield bb :: zz)
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(identity)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
@@ -34,4 +62,15 @@ object Either {
     try Right(a)
     catch { case e: Exception => Left(e) }
 
+  // Exercise 4.8 (text-only)
+  // I think that the best way to go would be to use a new data type. It would be similar to
+  // something like Either[List[Error], Success], but unlike either it would not short-circuit
+  // in the Left case.
+  // `orElse` would probably continue to work the same way, discarding its error list and
+  // instead returning a success value in the error case.
+  // `traverse` (and therefore `sequence`) would work a little differently, because they would
+  // not short-circuit when iterating through the list if they encounter a Left result. Rather,
+  // they would iterate through the entire list. On account of this, it might actually be better
+  // to implement this other `traverse` in terms of foldLeft instead of foldRight; short-circuiting
+  // isn't needed, but being stack-safe is a big plus.
 }
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,18 +1,41 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{Either => _, Option => _, Some => _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case Some(a) => Some(f(a))
+      case None => None
+    }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B =
+    this match {
+      case Some(a) => a
+      case None => default
+    }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    this match {
+      case Some(a) => f(a)
+      case None => None
+    }
+//   map(f) getOrElse None // the book's more elegant solution
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    this match {
+      case Some(_) => this
+      case None => ob
+    }
+//  this map (Some(_)) getOrElse ob // the book's more elegant solution
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] =
+    this match {
+      case Some(a) if f(a) => this
+      case _ => None
+    }
+//    flatMap(a => if (f(a)) Some(a) else None) // the book's more elegant solution
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +61,51 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = {
+    val transformed: Option[Seq[Double]] = mean(xs).map((m: Double) => xs.map(x => math.pow(x - m, 2)))
+    transformed flatMap mean
+    // the book had a SLIGHTLY more terse solution, where they did this on one line:
+//    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2)))) // book's solution
+  }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match {
+      case (Some(a0), Some(b0)) => Some(f(a0, b0))
+      case _ => None
+    }
+//  a flatMap (aa => b map (bb => f(aa, bb))) // the book's cleaner solution without pattern matching
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  def map3[A,B,C,D](a: Option[A], b: Option[B], c: Option[C])(f: (A, B, C) => D): Option[D] =
+    a flatMap(aa => map2(b, c)(f(aa, _, _)))
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case None :: as => None
+      case Some(aa) :: as => sequence(as) map (aa :: _)
+      case Nil => Some(Nil)
+    }
+  // The book's solution was similar, but combined two of my pattern matches into one.
+  // Overall I think the book's solution is better, because it's much less dependent on
+  // directly referencing data structures, so would more easily be generalized.
+//  a match {
+//    case Nil => Some(Nil)
+//    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+//  }
+  // The book also had a one-liner:
+//  a.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
+
+  def traverseInefficient[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    sequence(a map f)
+
+  // This solution of mine is based off the book's solution for sequence using foldRight. I'm
+  // afraid that I would not have come up with this by myself, but it makes some sense in retrospect:
+  // to build up an Option[List[B]] in one pass, given just a List[A] and an (A => Option[B]), we need
+  // to break down the structure of the list and accumulate it into a new Option. Breaking down the
+  // structure and accumulating *something* as we go is just what folds are good for. Meanwhile, we use
+  // map2 because it gives us a way to lift (::) into the Option context of our Option[B] and Option[List[B]].
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight(Some(Nil): Option[List[B]])((ha, z) => map2(f(ha), z)(_ :: _))
+
+  def sequenceReimpl[A](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(identity)
 }
